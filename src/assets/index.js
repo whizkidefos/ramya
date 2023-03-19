4e5b46438f80a67f303aa0c4a407120255c80a96
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2023-03-19 19:09:18 +0000

    Architecture, files and folder structure

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
+import ramya from './RamyaAI.png';
 
 export {
   download,
   logo,
   preview,
+  ramya
 };
--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
+    const navigation = useNavigate();
+    const [form, setForm] = useState({
+        name: '',
+        prompt: '',
+        photo: ''
+    });
+
+    const [generatingImg, setGeneratingImg] = useState(false);
+
   return (
     <div>CreatePost</div>
   )
