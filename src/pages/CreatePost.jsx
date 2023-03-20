3a7890d1ddac185ec694009742bd646fe2fdf3bb
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2023-03-20 20:06:29 +0000

    Added FormField component

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -14,9 +14,86 @@ const CreatePost = () => {
     });
 
     const [generatingImg, setGeneratingImg] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = () => {}
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.value]: e.target.value });
+    }
+
+    const handleSurpriseMe = () => {
+        const randomPrompt = getRandomPrompt(form.prompt);
+        setForm({ ...form, prompt: randomPrompt });
+    }
+
+    const generateImage = () => {}
 
   return (
-    <div>CreatePost</div>
+    <section className="max-w-7xl mx-auto">
+        <div className="">
+            <h1 className="mt-2 text-[32px] font-extrabold text-[#222328] max-w-[500px]">Create</h1>
+            <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">Create imaginative and visually stunning images through DALL-E AI, and share them with the community.</p>
+        </div>
+
+        <form className="mt-16 mx-w-3xl" onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-5">
+                <FormField
+                    labelName='Your name'
+                    type='text'
+                    name='name'
+                    placeholder='John Wick'
+                    value={form.name}
+                    handleChange={handleChange}
+                />
+                <FormField
+                    labelName='Prompt'
+                    type='text'
+                    name='prompt'
+                    placeholder='an armchair in the shape of an avocado'
+                    value={form.prompt}
+                    handleChange={handleChange}
+                    isSurpriseMe
+                    handleSurpriseMe={handleSurpriseMe}
+                />
+
+                <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+                    {form.photo ? (
+                        <img
+                            src={form.photo}
+                            alt={form.prompt}
+                            className='w-full h-full object-contain'
+                        />
+                    ) : (
+                        <img
+                            src={preview}
+                            alt='preview'
+                            className='w-9/12 h-9/12 object-contain'
+                        />
+                    )}
+
+                    {generatingImg && (
+                        <div className="absolute inset-0 z-0 flex justify-center items-center bg-[#0,0,0,0.5] rounded-lg">
+                            <Loader />
+                        </div>
+                    )}
+                </div>
+            </div>
+
+            <div className="mt-5 flex gap-5">
+                <button type='button' onClick={generateImage} className="text-white bg-[#143151] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+                    {generatingImg ? 'Generating...' : 'Generate'}
+                </button>
+            </div>
+
+            <div className="mt-10">
+                <p className='mt-2 text-[#666e75] text-[14px]'>Once you have created the image you want, you can share it with others in the community.</p>
+                <button type="submit" className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'>
+                    {loading ? 'Sharing...' : 'Share with the community'}
+                </button>
+            </div>
+        </form>
+    </section>
   )
 }
 
